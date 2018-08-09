317ebfd63605bcf067bc135d00659860f087d9ee
Nathan Huberty <nathan.huberty@orange.fr>
2018-08-09 17:47:29 +0200

    select 2 ok

---
+++ app/javascript/components/select2.js
@@ -0,0 +1,10 @@
+// app/javascript/components/select2.js
+import $ from 'jquery';
+import 'select2';
+
+$('.select2').select2();
+
+// Requiring CSS! Path is relative to ./node_modules
+import 'select2/dist/css/select2.css';
+
+console.log('Hello World from select2')
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,3 +9,5 @@
 
 import 'bootstrap';
 console.log('Hello World from Webpacker')
+
+import '../components/select2';
